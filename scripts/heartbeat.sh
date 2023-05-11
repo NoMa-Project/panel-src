5a305244421096bbb88687341f5bb7e772b57a4c
MazBaz <mrlog42@gmail.com>
2023-05-11 12:25:05 +0200

    edit sh

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -8,5 +8,5 @@ PORT=$4
 URL=$7
 
 sshpass -p "$PASSWORD" ssh -T -p "$PORT" "$USER"@10.211.55.13 << EOF
-  curl -X POST http://10.69.19.24:8043/api/heartbeat/8/a86a51e5d943d8e37d2636d345fcad25
+  curl -X POST $url
 EOF
